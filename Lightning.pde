4bf41446278867f75f3fb9e60a63a4fe5aa35491
unknown <clchen@697-334-Thorin.sf.k12.internal>
2014-09-09 10:36:00 -0700

    Lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,78 @@
+int col;
+int rot;
 void setup()
 {
-  size(300,300);
+ size(800,800);
+ colorMode(HSB,360);
 }
 void draw()
 {
-
+ fill(0,0,0,36);
+ stroke(0,0,0,36);
+ rect(0,0,1280,960);
 }
 void mousePressed()
 {
-
+ Bolt bob = new Bolt(mouseX,mouseY,col,rot);
+ bob.show();
 }
-
+void keyPressed()
+{
+ if (key == CODED) {
+    switch (keyCode)
+    {
+       case UP : 
+      {rot--;
+       break;
+      }
+       case DOWN : 
+      {rot++;
+       break;
+      }
+      case LEFT : 
+      {col--;
+       col%=120;
+       while(col<0)
+       {col+=120;}
+       break;
+      }
+       case RIGHT : 
+      {col++;
+       col%=120;
+       while(col<0)
+       {col+=120;}
+       break;
+      }
+	}
+ }
+}
+class Bolt {
+ int x;
+ int y;
+ int c;
+ int r;
+ Bolt (int x, int y, int c, int r) {
+ this.x = x;
+ this.y = y;
+ this.c = c;
+ this.r = r;
+ }
+ void show()
+ {
+  stroke(3*c,360,180);
+  translate(x, y);
+  rotate(r*PI/60);
+  int startX = 0;
+  int startY = 0;
+  int endX;
+  int endY;
+  while(startX<800 && startY<400 && startY>-400)
+  {
+   endX = startX + (int)(Math.random()*10+1);
+   endY = startY + (int)(Math.random()*11-5);
+   line(startX, startY, endX, endY);
+   startX = endX;
+   startY = endY;
+  }
+ }
+}
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,85 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int col;
+int rot;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+ size(800,800);
+ colorMode(HSB,360);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+ fill(0,0,0,36);
+ stroke(0,0,0,36);
+ rect(0,0,1280,960);
+ noFill();
 }
 public void mousePressed()
 {
-  redraw();
+ Bolt bob = new Bolt(mouseX,mouseY,col,rot);
+ bob.show();
 }
-
+public void keyPressed()
+{
+ if (key == CODED) {
+    switch (keyCode)
+    {
+       case UP : 
+      {rot--;
+       break;
+      }
+       case DOWN : 
+      {rot++;
+       break;
+      }
+      case LEFT : 
+      {col--;
+       col%=120;
+       while(col<0)
+       {col+=120;}
+       break;
+      }
+       case RIGHT : 
+      {col++;
+       col%=120;
+       while(col<0)
+       {col+=120;}
+       break;
+      }
+	}
+ }
+}
+class Bolt {
+ int x;
+ int y;
+ int c;
+ int r;
+ Bolt (int x, int y, int c, int r) {
+ this.x = x;
+ this.y = y;
+ this.c = c;
+ this.r = r;
+ }
+ public void show()
+ {
+  stroke(3*c,360,180);
+  translate(x, y);
+  rotate(r*PI/60);
+  int startX = 0;
+  int startY = 0;
+  int endX;
+  int endY;
+  while(startX<800 && startY<400 && startY>-400)
+  {
+   endX = startX + (int)(Math.random()*10+1);
+   endY = startY + (int)(Math.random()*11-5);
+   line(startX, startY, endX, endY);
+   startX = endX;
+   startY = endY;
+  }
+ }
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
